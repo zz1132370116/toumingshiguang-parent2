25baa4f3fe38d227db93b8a931a53aeea4ea9d9f
张磊 <44484672+zz1132370116@users.noreply.github.com>
2019-03-19 15:17:47 +0800

    修改配置

--- toumingshiguang-service/src/main/java/com/czxy/toumingshiguang/controller/AddressController.java
+++ toumingshiguang-service/src/main/java/com/czxy/toumingshiguang/controller/AddressController.java
@@ -63,6 +63,8 @@ public class AddressController {
     @PostMapping("/deleted")
     public ResponseEntity<BaseResult> deleted (@RequestBody Address address){
         addressService.delete(address);
+        System.out.println("aaa");
+        System.out.println("aaa");
         return ResponseEntity.ok(new BaseResult(0,"成功"));
 
     }
